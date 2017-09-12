fcf2904f29e9b21e2704261eda62794aad518165
liu-feng <w710989327@foxmail.com>
2017-09-12 18:04:22 +0800

    upload bintray. version 0.1.3. fix tabhost bug

---
+++ geetionlib/src/main/java/com/letion/geetionlib/util/TIO.java
@@ -0,0 +1,48 @@
+package com.letion.geetionlib.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * Created by liu-feng on 2017/6/27.
+ */
+public class TIO {
+    private TIO() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * 关闭IO
+     *
+     * @param closeables closeables
+     */
+    public static void closeIO(final Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 安静关闭IO
+     *
+     * @param closeables closeables
+     */
+    public static void closeIOQuietly(final Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+}
